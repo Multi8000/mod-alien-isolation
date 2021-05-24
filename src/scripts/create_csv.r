0aff905a944d9baa3bf7ee5d43f021e490274dfb
Multi8000 <marcos.martins0@outlook.com>
2021-05-24 20:55:20 -0300

    feat: add a .csv with a list of the mod changes

---
+++ src/scripts/create_csv.r
@@ -0,0 +1,24 @@
+install.packages("here")
+install.packages("data.table")
+install.packages("readxl")
+require("here")
+require("data.table")
+require("readxl")
+
+
+# Read Excel (.xlsx) files
+audio_changes <- readxl::read_xlsx(path = "Audio.xlsx", col_types = c("text"))
+legend_changes <- readxl::read_xlsx(path = "Legenda.xlsx", col_types = c("text"))
+
+
+# Convert Excel (.xlsx) files to .csv
+setwd(here())
+
+fwrite(x = audio_changes,
+       file = "../../../Documents/GitHub Repositories/mod-alien-isolation/src/audio_changes.csv",
+       sep = ",",
+       quote = TRUE)
+fwrite(x = legend_changes,
+       file = "../../../Documents/GitHub Repositories/mod-alien-isolation/src/legend_changes.csv",
+       sep = ",",
+       quote = TRUE)
